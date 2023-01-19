87fd8f45317efbfeba8393af463b5893ea9dacd6
MontaCoder <montassarhajri@outlook.com>
2023-01-19 22:04:18 +0100

    small change

--- SingleWebPageAngularJS/Module-1-Coding-Assignment/app.js
+++ SingleWebPageAngularJS/Module-1-Coding-Assignment/app.js
@@ -1,30 +1,33 @@
-(function () {
-'use strict';
+(function() {
+    'use strict';
 
-angular.module('LunchCheck', [])
-.controller('LunchCheckController', LunchCheckController);
+    angular.module('LunchCheck', [])
+        .controller('LunchCheckController', LunchCheckController);
 
-LunchCheckController.$inject = ['$scope'];
+    LunchCheckController.$inject = ['$scope'];
 
-function LunchCheckController ($scope) {
-  $scope.lunchItems = "";
-  $scope.message = "";
+    function LunchCheckController($scope) {
+        $scope.dishes = '';
+        $scope.message = '';
+        $scope.checked = false;
 
-  $scope.checkLunchItems = function() {
-    if ($scope.lunchItems == "") {
-      $scope.message = "Please enter data first";
+        $scope.checkLunch = function() {
+            if ($scope.dishes.trim().length === 0) {
+                $scope.empty = true;
+            } else {
+                $scope.checked = true;
+                $scope.empty = false;
+                var arrayDishes = $scope.dishes.split(',');
+                var arrayDishesWithoutEmptys = arrayDishes.filter(function(v) {
+                    return v.trim() !== '';
+                });
+
+                if (arrayDishesWithoutEmptys.length <= 3) {
+                    $scope.message = 'Enjoy!';
+                } else {
+                    $scope.message = 'Too much!';
+                }
+            }
+        };
     }
-    else {
-      const items = $scope.lunchItems.split(',').filter(e => e.trim() != "");
-
-      if (items.length > 3) {
-        $scope.message = "Too much!";
-      } else {
-        $scope.message = "Enjoy!";
-      }
-    }
-
-  }
-}
-
 })();
